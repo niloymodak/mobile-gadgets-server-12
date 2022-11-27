51bc1bc3d647524c3a0065d4e85354cb9c264fca
niloymodak <niloymodak5@gmail.com>
2022-11-27 14:25:36 +0600

    seven commit

--- index.js
+++ index.js
@@ -150,6 +150,23 @@ async function run() {
             res.send(result);
         });
 
+        app.post('/create-payment-intent', async (req, res) => {
+            const booking = req.body;
+            const price = booking.price;
+            const amount = price * 100;
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                currency: 'usd',
+                amount: amount,
+                "payment_method_types": [
+                    "card"
+                ]
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        });
+
         app.get('/jwt', async (req, res) => {
             const email = req.query.email;
             const query = { email: email };
